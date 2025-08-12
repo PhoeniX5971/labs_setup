51ebc7552d67bd45ae499698deb111e5b1d7eedb
PhoeniX5971 <moh845195@gmail.com>
2025-08-12 19:22:49 +0300

    finished nessus first try

--- scripts/setup/installers/nessus.ps1
+++ scripts/setup/installers/nessus.ps1
@@ -1,12 +1,11 @@
 Write-Host "[*] Downloading nessus." -ForegroundColor Yellow
 
-curl --request GET \
-  --url 'https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.9.2-x64.msi' \
-  --output 'Nessus-10.9.2-x64.msi';
-
+iwr "https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.9.2-x64.msi" -OutFile "Nessus-10.9.2-x64.msi"
 
 Write-Host "[+] Nessus successfully downloaded." -ForegroundColor Green
 
-msiexec.exe /i "Nessus-10.9.2-x64.msi" /qn /norestart /l*v "$PWD\nessus_install.log"
+Write-Host "[*] Installing on disk." -ForegroundColor Yellow
 
-Write-Host "[*] Installing on disk." -ForegroundColor Green
+msiexec.exe /i "Nessus-10.9.2-x64.msi" /qn /norestart
+
+Write-Host "[+] Finished Installation." -ForegroundColor Green
