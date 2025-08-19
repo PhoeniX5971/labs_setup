1d87b04cb8b0b730e9147b436a2cefda45677a07
PhoeniX5971 <moh845195@gmail.com>
2025-08-19 21:32:36 +0300

    fix in velociraptor server install

--- scripts/installers/velociraptor/server_install.sh
+++ scripts/installers/velociraptor/server_install.sh
@@ -80,7 +80,6 @@ sudo mkdir -p "$INSTALL_DIR"
 # Generate server config
 echo "[*] Generating server configuration..."
 sudo ./"$FILENAME" config generate \
-	--non-interactive \
 	--server \
 	--config "${INSTALL_DIR}/server.config.yaml" \
 	--port 8889 \
@@ -92,7 +91,6 @@ echo "[+] Velociraptor server configuration generated successfully."
 # Generate client config
 echo "[*] Generating client configuration..."
 sudo ./"$FILENAME" config generate \
-	--non-interactive \
 	--client \
 	--config "${INSTALL_DIR}/client.config.yaml" \
 	--server "${INSTALL_DIR}/server.config.yaml" \
